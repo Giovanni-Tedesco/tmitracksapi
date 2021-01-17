f70d26f2167cd1ee257d61ebb5ec09cfc6a7aad6
Giovanni Tedesco <giotedesco33@gmail.com>
2021-01-17 12:21:57 -0500

    Changes db uri to be an environment variable

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
 	"time"
 
 	api "github.com/Giovanni-Tedesco/tmitracksapi/internal"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -12,10 +15,17 @@ import (
 func main() {
 	a := &api.App{}
 
+	err := godotenv.Load()
+	uri := os.Getenv("MONGO_DB_URI")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
